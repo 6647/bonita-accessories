10b1599861a1e342c381dca83df83f5752a8ad5b
= <=>
2025-04-19 21:44:38 +0300

    main design

---
+++ main.js
@@ -0,0 +1,548 @@
+const products = [
+    // ANKLET
+    {
+        id: 601,
+        title: "خلخال",
+        price: "25 جنيه",
+        categories: ["anklet"],
+        description: '',
+        image: "imgs/anklet/anklet-1 25egy.jpeg"
+    },
+
+    // BRACELETS
+    {
+        id: 602,
+        title: "أسورة",
+        price: "15 جنيه",
+        categories: ["bracelets"],
+        description: '',
+        image: "imgs/bracelets/bracelets-1 15egy.jpeg"
+    },
+    {
+        id: 603,
+        title: "أسورة",
+        price: "25 جنيه",
+        categories: ["bracelets"],
+        description: 'متاح اي لون',
+        image: "imgs/bracelets/bracelets-2-anycolor 25egy.jpeg"
+    },
+    
+    {
+        id: 604,
+        title: "أسورة",
+        price: "25 جنيه",
+        categories: ["bracelets"],
+        description: 'متاح اي لون',
+        image: "imgs/bracelets/bracelets-3-anycolor 25egy.jpeg"
+    },
+    {
+        id: 605,
+        title: "أسورة",
+        price: "10 جنيه",
+        categories: ["bracelets"],
+        description: 'دلع طفلك باكسسوارة ايد',
+        image: "imgs/bracelets/bracelets-3-baby 10egy.jpeg"
+    },
+    {
+        id: 606,
+        title: "أسورة",
+        price: "20 جنيه",
+        categories: ["bracelets"],
+        description: '',
+        image: "imgs/bracelets/bracelets-4 20egy.jpeg"
+    },
+    {
+        id: 607,
+        title: "أسورة",
+        price: "10 جنيه",
+        categories: ["bracelets"],
+        description: '',
+        image: "imgs/bracelets/bracelets-7 10.jpeg"
+    },
+    {
+        id: 608,
+        title: "أسورة",
+        price: "15 جنيه",
+        categories: ["bracelets"],
+        description: '',
+        image: "imgs/bracelets/bracelets-9 15egy.jpeg"
+    },
+    {
+        id: 609,
+        title: "أسورة",
+        price: "10 جنيه",
+        categories: ["bracelets"],
+        description: '',
+        image: "imgs/bracelets/bracelets-10 10egy.jpeg"
+    },
+    {
+        id: 610,
+        title: "أسورة",
+        price: "15 جنيه",
+        categories: ["bracelets"],
+        description: 'متاح اي لون',
+        image: "imgs/bracelets/bracelets-anycolor-7 15egy.jpeg"
+    },
+    {
+        id: 611,
+        title: "أسورة",
+        price: "10 جنيه",
+        categories: ["bracelets"],
+        description: 'متاح اي لون',
+        image: "imgs/bracelets/bracelets-anycolor-14 10egy.jpeg"
+    },
+    {
+        id: 612,
+        title: "أسورة",
+        price: "10 جنيه",
+        categories: ["bracelets"],
+        description: 'اسورة قلب، متاح اي حرف',
+        image: "imgs/bracelets/bracelets-hearts-anyletter 10egy.jpeg"
+    },
+    {
+        id: 613,
+        title: "أسورة مطاط",
+        price: "25 جنيه",
+        categories: ["bracelets"],
+        description: '',
+        image: "imgs/bracelets/bracelets-rubber-5 25egy.jpeg"
+    },
+    {
+        id: 614,
+        title: "أسورة مطاط",
+        price: "10 جنيه",
+        categories: ["bracelets"],
+        description: '',
+        image: "imgs/bracelets/bracelets-rubber-6 10egy.jpeg"
+    },
+    {
+        id: 615,
+        title: "أسورة",
+        price: "25 جنيه",
+        categories: ["bracelets"],
+        description: 'اسورة باسم طفلك او طفلتك',
+        image: "imgs/bracelets/bracelets-with-naming-babies 25egy.jpeg"
+    },
+    {
+        id: 616,
+        title: "أسورة",
+        price: "30 جنيه",
+        categories: ["bracelets"],
+        description: 'اسورة بفصوص',
+        image: "imgs/bracelets/bracelets-with-stones-1 30egy.jpeg"
+    },
+    {
+        id: 617,
+        title: "أسورة",
+        price: "30 جنيه",
+        categories: ["bracelets"],
+        description: 'اسورة بفصوص',
+        image: "imgs/bracelets/bracelets-with-stones-2 30 egy.jpeg"
+    },
+    
+    // EARRINGS
+    {
+        id: 618,
+        title: "حلق",
+        price: "15 جنيه",
+        categories: ["earrings"],
+        description: 'حلق دبدوب، متاح كل الالوان',
+        image: "imgs/earrings/earrings-bear-anycolor-2 15egy.jpeg"
+    },
+    {
+        id: 619,
+        title: "حلق",
+        price: "15 جنيه",
+        categories: ["earrings"],
+        description: 'حلق الفراشة',
+        image: "imgs/earrings/earrings-butterfly-3 15egy.jpeg"
+    },
+    {
+        id: 620,
+        title: "حلق لماع",
+        price: "20 جنيه",
+        categories: ["earrings"],
+        description: 'متاح الوان تانية',
+        image: "imgs/earrings/earrings-shiny-ava-manycolors-3 20egy.jpeg"
+    },
+    {
+        id: 621,
+        title: "حلق",
+        price: "30 جنيه",
+        categories: ["earrings"],
+        description: '',
+        image: "imgs/earrings/earrings1-20egy.jpeg"
+    },
+
+    // MOB & KEYCHAINS
+    {
+        id: 622,
+        title: "علاقة مفاتيح",
+        price: "10 جنيه",
+        categories: ["mob-keychain"],
+        description: '',
+        image: "imgs/mob-keychains/keychain-1 10egy.jpeg"
+    },
+    {
+        id: 623,
+        title: "علاقة وميدالية",
+        price: "25 جنيه",
+        categories: ["mob-keychain"],
+        description: '',
+        image: "imgs/mob-keychains/mob-1 25.jpeg"
+    },
+    {
+        id: 624,
+        title: "علاقة وميدالية",
+        price: "25 جنيه",
+        categories: ["mob-keychain"],
+        description: '',
+        image: "imgs/mob-keychains/mob-2 25egy.jpeg"
+    },
+    {
+        id: 625,
+        title: "علاقة وميدالية",
+        price: "25 جنيه",
+        categories: ["mob-keychain"],
+        description: '',
+        image: "imgs/mob-keychains/mob-3 25egy.jpeg"
+    },
+    {
+        id: 626,
+        title: "علاقة وميدالية",
+        price: "25 جنيه",
+        categories: ["mob-keychain"],
+        description: '',
+        image: "imgs/mob-keychains/mob-4 25egy.jpeg"
+    },
+    {
+        id: 627,
+        title: "علاقة وميدالية",
+        price: "25 جنيه",
+        categories: ["mob-keychain"],
+        description: '',
+        image: "imgs/mob-keychains/mob-8 25egy.jpeg"
+    },
+    {
+        id: 628,
+        title: "علاقة وميدالية",
+        price: "25 جنيه",
+        categories: ["mob-keychain"],
+        description: '',
+        image: "imgs/mob-keychains/mob-9 25egy.jpeg"
+    },
+    {
+        id: 629,
+        title: "علاقة وميدالية",
+        price: "25 جنيه",
+        categories: ["mob-keychain"],
+        description: '',
+        image: "imgs/mob-keychains/mob-10 25egy.jpeg"
+    },
+    {
+        id: 630,
+        title: "علاقة وميدالية",
+        price: "25 جنيه",
+        categories: ["mob-keychain"],
+        description: '',
+        image: "imgs/mob-keychains/mob-11 25egy.jpeg"
+    },
+    {
+        id: 631,
+        title: "علاقة وميدالية",
+        price: "25 جنيه",
+        categories: ["mob-keychain"],
+        description: '',
+        image: "imgs/mob-keychains/mob-12 25egy.jpeg"
+    },
+    {
+        id: 632,
+        title: "علاقة وميدالية",
+        price: "25 جنيه",
+        categories: ["mob-keychain"],
+        description: 'متاح اي اسم',
+        image: "imgs/mob-keychains/mob-anyname-6 25egy.jpeg"
+    },
+    {
+        id: 633,
+        title: "علاقة وميدالية",
+        price: "25 جنيه",
+        categories: ["mob-keychain"],
+        description: 'متاح اي اسم او لون او شكل',
+        image: "imgs/mob-keychains/mob-full-custom-7 25egy.jpeg"
+    },
+    {
+        id: 634,
+        title: "علاقة وميدالية",
+        price: "10 جنيه",
+        categories: ["mob-keychain"],
+        description: 'بجمال كلمة امي ❤️',
+        image: "imgs/mob-keychains/mob-mother-13 10egy.jpeg"
+    },
+    {
+        id: 635,
+        title: "علاقة وميدالية",
+        price: "30 جنيه",
+        categories: ["mob-keychain"],
+        description: 'متاح تغيير اللون او الشكل',
+        image: "imgs/mob-keychains/mob-mother-full-custom-5 30egy.jpeg"
+    },
+
+    // NECKLACES 
+    {
+        id: 636,
+        title: "سلسلة",
+        price: "20 جنيه",
+        categories: ["necklaces"],
+        description: 'لست الحبايب 💕',
+        image: "imgs/necklaces/my-mom 20egy.jpeg"
+    },
+    {
+        id: 637,
+        title: "سلسلة",
+        price: "20 جنيه",
+        categories: ["necklaces"],
+        description: 'بكلمة أمي',
+        image: "imgs/necklaces/my-mom2 20egy.jpeg"
+    },
+    {
+        id: 638,
+        title: "سلسلة",
+        price: "20 جنيه",
+        categories: ["necklaces"],
+        description: '',
+        image: "imgs/necklaces/necklaces-1 20egy.jpeg"
+    },
+    {
+        id: 639,
+        title: "سلسلة",
+        price: "20 جنيه",
+        categories: ["necklaces"],
+        description: '',
+        image: "imgs/necklaces/necklaces-5 20egy.jpeg"
+    },
+    {
+        id: 640,
+        title: "سلسلة",
+        price: "20 جنيه",
+        categories: ["necklaces"],
+        description: 'بحروف من اختيارك',
+        image: "imgs/necklaces/necklaces-couples-2 20egy.jpeg"
+    },
+    {
+        id: 641,
+        title: "سلسلة",
+        price: "30 جنيه",
+        categories: ["necklaces"],
+        description: ' شكل الدايموند المميز 💎 الوان تانية متاحة',
+        image: ""
+    },
+    {
+        id: 642,
+        title: "سلسلة",
+        price: "15 جنيه",
+        categories: ["necklaces"],
+        description: 'نجمة البحر',
+        image: "imgs/necklaces/necklaces-sea-star-2 15egy.jpeg"
+    },
+    {
+        id: 643,
+        title: "سلسلة",
+        price: "20 جنيه",
+        categories: ["necklaces"],
+        description: 'متاح اشكال مختلفة',
+        image: "imgs/necklaces/necklaces-star-ava-othershapes-4 20egy.jpeg"
+    },
+
+    // PINS
+    {
+        id: 644,
+        title: "دبوس",
+        price: "5 جنيه",
+        categories: ["pins"],
+        description: '',
+        image: "imgs/pin/pin-1 5egy.jpeg"
+    },
+
+    // RINGS
+    {
+        id: 645,
+        title: "خاتم اطفال",
+        price: "5 جنيه",
+        categories: ["rings"],
+        description: '',
+        image: "imgs/rings/ring-kids 5egy.jpeg"
+    },
+    
+    // SEBHA 
+    {
+        id: 646,
+        title: "سبحة 33 حبة",
+        price: "30 جنيه",
+        categories: ["sebha"],
+        description: '',
+        image: "imgs/sebha/sebha33-1 30egy.jpeg"
+    },
+    {
+        id: 647,
+        title: "سبحة 33 حبة",
+        price: "50 جنيه",
+        categories: ["sebha"],
+        description: '',
+        image: "imgs/sebha/sebha33-3 50egy.jpeg"
+    },
+    {
+        id: 648,
+        title: "سبحة 99 حبة",
+        price: "110 جنيه",
+        categories: ["sebha"],
+        description: 'متاح اي لون',
+        image: "imgs/sebha/sebha99-anycolor 110egy.jpeg"
+    },
+
+    // SETS & OTHERS
+    {
+        id: 649,
+        title: "اسورة",
+        price: "40 جنيه",
+        categories: ["sets", "bracelets"],
+        description: 'اقرب اتنين 💕',
+        image: "imgs/best-friend-any-colors 40egy.jpeg"
+    },
+    {
+        id: 650,
+        title: "طقم",
+        price: "35 جنيه",
+        categories: ["sets"],
+        description: '',
+        image: "imgs/collection-1 35egy.jpeg"
+    },
+    {
+        id: 651,
+        title: "طقم",
+        price: "75 جنيه",
+        categories: ["sets"],
+        description: '',
+        image: "imgs/collection-2 75egy.jpeg"
+    },
+    {
+        id: 652,
+        title: "طقم",
+        price: "25 جنيه",
+        categories: ["sets"],
+        description: '',
+        image: "imgs/pack-bracelets&necklaces-1 25egy.jpeg"
+    },
+    {
+        id: 653,
+        title: "طقم",
+        price: "15 جنيه",
+        categories: ["sets"],
+        description: 'للاطفال حلق واسورة',
+        image: "imgs/ring&bracelets-1-kids 15egy.jpeg"
+    },
+    {
+        id: 654,
+        title: "طقم",
+        price: "45 جنيه",
+        categories: ["sets"],
+        description: 'طقم مميز مقاوم لتغيير اللون',
+        image: "imgs/steel-pack-not-changing-color 45egy.jpeg"
+    },
+];
+// WhatsApp contact number
+const whatsappNumber = "966501234567";
+
+// Initialize the page
+document.addEventListener('DOMContentLoaded', function () {
+    // Show splash screen for 3 seconds
+    setTimeout(function () {
+        const splashScreen = document.getElementById('splashScreen');
+        const mainContent = document.getElementById('mainContent');
+
+        splashScreen.style.opacity = '0';
+        splashScreen.style.visibility = 'hidden';
+
+        mainContent.style.opacity = '1';
+
+        renderProducts(products);
+        setupFilterButtons();
+    }, 100);
+
+});
+
+// Render products to the page
+function renderProducts(productsToRender) {
+    const productsContainer = document.getElementById('productsContainer');
+    
+    if (productsToRender.length === 0) {
+        productsContainer.innerHTML = '<div class="no-products">No products found in this category.</div>';
+        return;
+    }
+    
+    let html = '';
+    
+    productsToRender.forEach(product => {
+        // Convert categories array to space-separated string for data attribute
+        const categoryClasses = product.categories.join(' ');
+        
+        html += `
+            <div class="product-card" data-categories="${categoryClasses}">
+                <div class="product-image-container">
+                    <img src="${product.image}" alt="${product.title}" class="product-image" loading="lazy">
+                </div>
+                <div class="product-info">
+                    <h3 class="product-title">${product.title}</h3>
+                    <p class="product-title">منتج رقم ${product.id}</hp>
+                    <h3 class="product-description">${product.description}</h3>
+                    <div class="product-price">${product.price}</div>
+                    <button class="whatsapp-btn" onclick="contactAboutProduct(${product.id})">
+                        <span class="whatsapp-icon">📱</span> Order via WhatsApp
+                    </button>
+                </div>
+            </div>
+        `;
+    });
+    
+    productsContainer.innerHTML = html;
+}
+
+// Set up filter button functionality
+function setupFilterButtons() {
+    const filterButtons = document.querySelectorAll('.filter-btn');
+
+    filterButtons.forEach(button => {
+        button.addEventListener('click', function () {
+            filterButtons.forEach(btn => btn.classList.remove('active'));
+            this.classList.add('active');
+
+            const category = this.dataset.category;
+            filterProducts(category);
+        });
+    });
+}
+
+// Filter products by category
+function filterProducts(category) {
+    if (category === 'all') {
+        renderProducts(products);
+        return;
+    }
+    
+    const filteredProducts = products.filter(product => 
+        product.categories.includes(category)
+    );
+    renderProducts(filteredProducts);
+}
+
+// Handle WhatsApp contact for a product
+function contactAboutProduct(productId) {
+    const product = products.find(p => p.id === productId);
+    if (!product) return;
+
+    const message = `Hello Bonita Accessories! I'm interested in purchasing your ${product.title} (${product.price}). Please provide more details.`;
+    const encodedMessage = encodeURIComponent(message);
+    const whatsappUrl = `https://wa.me/${whatsappNumber}?text=${encodedMessage}`;
+
+    window.open(whatsappUrl, '_blank');
+}
